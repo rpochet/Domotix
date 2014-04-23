97466e6f44340dc2132a83e786fdea8c58f646d1
rpochet <pochet.romuald@gmail.com>
2014-04-23 22:51:35 +0200

    Create app.js

---
+++ www/server/rpc/app.js
@@ -0,0 +1,20 @@
+exports.actions = function(req, res, ss){
+
+  // Easily debug incoming requests here
+  console.log(req);
+
+  return {
+  
+    // Square a number and return the result
+    square: function(num){
+      res(num * num);
+    }
+
+    sendAlert: function(){
+      ss.publish.all('systemAlert',
+        'The server is about to be shut down'
+      );
+    }
+
+  }
+}
